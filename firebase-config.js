1495cdb6152fd4a6a7b0a7fe654f6edbf00c9a36
vdsperera <kadvsperera@gmail.com>
2026-07-19 13:52:34 +0000

    Deploying to gh-pages from @ vdsperera/viper-hunt@7c85003b11c21e399b8d37b03af3c791fba55476 🚀

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration example
+// Copy this file to  and fill in your actual settings from the Firebase Console.
+export const firebaseConfig = {
+    apiKey: AIzaSyCzJ5ARWXXG3PQXnz_6zs-9OZxw9jzvUqE,
+    authDomain: viper-hunt.firebaseapp.com,
+    projectId: viper-hunt,
+    storageBucket: viper-hunt.firebasestorage.app,
+    messagingSenderId: 722020252807,
+    appId: 1:722020252807:web:cbc97cdc7fad6ea12d4c22,
+    measurementId: G-RFP8T8G2ML
+};
+
