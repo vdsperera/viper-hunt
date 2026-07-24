1856e6038b139e0dffb37a5b7145254e1ea1bb5b
vdsperera <kadvsperera@gmail.com>
2026-07-24 06:47:33 +0000

    Deploying to gh-pages from @ vdsperera/viper-hunt@0efce4677b0f1e401735347a3bf27b3ea7175f99 🚀

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,11 @@ export const firebaseConfig = {
     // Set useCloudConfig to false for local testing (uses local default rules directly),
     // or true to fetch and sync live rules with Firebase Firestore.
     useCloudConfig: true,
-    apiKey: AIzaSyCzJ5ARWXXG3PQXnz_6zs-9OZxw9jzvUqE,
-    authDomain: viper-hunt.firebaseapp.com,
-    projectId: viper-hunt,
-    storageBucket: viper-hunt.firebasestorage.app,
-    messagingSenderId: 722020252807,
-    appId: 1:722020252807:web:cbc97cdc7fad6ea12d4c22,
-    measurementId: G-RFP8T8G2ML
+    apiKey: "AIzaSyCzJ5ARWXXG3PQXnz_6zs-9OZxw9jzvUqE",
+    authDomain: "viper-hunt.firebaseapp.com",
+    projectId: "viper-hunt",
+    storageBucket: "viper-hunt.firebasestorage.app",
+    messagingSenderId: "722020252807",
+    appId: "1:722020252807:web:cbc97cdc7fad6ea12d4c22",
+    measurementId: "G-RFP8T8G2ML"
 };
-
